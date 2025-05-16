6b1f6581d2ac5e1c48f6bb34394c87f355ad4c86
Ina-Verena Ludwig <Ina-Verena.Ludwig@nea-x.de>
2025-05-16 12:00:30 +0200

    first commit

---
+++ altego/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ width: "400px", height: "600px" }} className="hologram-bg">
+      <div style={{ padding: "8px" }}>Hallo Welt</div>
+    </div>
+  );
+}
+
+export default App;
