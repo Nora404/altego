ba3feee1ad4cfbd9e7c1d73c2631b89d7177f710
ina-verena <144120729+ina-verena@users.noreply.github.com>
2025-05-18 22:39:57 +0200

    with lines

--- altego/src/Layout/Content/Content.tsx
+++ altego/src/Layout/Content/Content.tsx
@@ -5,15 +5,15 @@ const Content = () => {
     return (
         <div className="content-grid">
             <div className="corner top-left" />
-            <div className="edge top" />
+            <div className="edge-top" />
             <div className="corner top-right" />
-            <div className="edge left" />
+            <div className="edge-left" />
             <div className="content glass-dark">
                 <Outlet />
             </div>
-            <div className="edge right" />
+            <div className="edge-right" />
             <div className="corner bottom-left" />
-            <div className="edge bottom" />
+            <div className="edge-bottom" />
             <div className="corner bottom-right" />
         </div>
 
