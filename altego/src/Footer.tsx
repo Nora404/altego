aa1c31c65a35ce506740e3809f71f499f2a6c549
ina-verena <144120729+ina-verena@users.noreply.github.com>
2025-05-18 15:40:12 +0200

    add sozial icons

--- altego/src/Footer.tsx
+++ altego/src/Footer.tsx
@@ -7,10 +7,15 @@ const Footer = () => {
             <div style={{ height: "60px" }} />
 
             <div className="left-deco" />
-            <div className="right-deco" />
+            <div className="right-deco">
+                <img src="assets/facebookklon.png" alt="icon" style={{ width: "24px", height: "24px" }} />
+                <img src="assets/twitterklon.png" alt="icon" style={{ width: "24px", height: "24px" }} />
+                <img src="assets/youtubeklon.png" alt="icon" style={{ width: "24px", height: "24px" }} />
+                <img src="assets/twitchklon.png" alt="icon" style={{ width: "24px", height: "24px", paddingRight: "10px" }} />
+            </div>
             <footer className="footer-fixed">
                 {/* Inhalt */}
-                <p style={{ margin: 0 }}>© 2025 Dein Projekt</p>
+                <p style={{ margin: 0 }}>Kontakt - Jobs - Impressum - Datenschutz</p>
             </footer>
         </>
     );
