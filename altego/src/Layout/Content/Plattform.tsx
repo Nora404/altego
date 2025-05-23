3562ecdb5c086e0f9926c04baa8b978787254136
Ina-Verena Ludwig <Ina-Verena.Ludwig@nea-x.de>
2025-05-23 15:48:30 +0200

    shot platform

--- altego/src/Layout/Content/Plattform.tsx
+++ altego/src/Layout/Content/Plattform.tsx
@@ -1,22 +1,30 @@
-import type { ReactNode } from "react";
+import { useState, type ReactNode } from "react";
 import "./Plattform.css";
 
 type PlattformProps = {
-    children: ReactNode;
-    title?: string;
+  children: ReactNode;
+  title?: string;
 };
 
 export const Plattform = ({ children, title = "" }: PlattformProps) => {
-    return (
-        <div className="platform-container flex-col-center float-y-animation">
-            <div className="plattform-ad hologram-background outlined-text ">
-                <h3>{title}</h3>
-                {children}
-            </div>
-            <div className="platform">
-                <img src="/assets/plattform.svg" alt="ad" className="plattform-img" />
-            </div>
-        </div>
-    );
-};
+  const [active, setActive] = useState<boolean>(true);
 
+  const handleClick = () => {
+    setActive((prev) => !prev);
+  };
+
+  return (
+    <div className="platform-container flex-col-center float-y-animation">
+      <div className="plattform-ad hologram-background outlined-text ">
+        <h3>{title}</h3>
+        {children}
+      </div>
+      <button
+        onClick={handleClick}
+        className={`platform ${active && "platform-active"}`}
+      >
+        <img src="/assets/plattform.svg" alt="ad" className="plattform-img" />
+      </button>
+    </div>
+  );
+};
