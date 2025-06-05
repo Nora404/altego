20ded99bef5e2056bde1d5c51cac080876dc6045
ina-verena <144120729+ina-verena@users.noreply.github.com>
2025-06-05 08:37:29 +0200

    new vision

---
+++ altego-v2/src/App.tsx
@@ -0,0 +1,81 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="grid-container">
+      <div className="layout-header">
+        <div className="header-grid">
+          <div className="header-logo">logo</div>
+          <div className="header-title">header</div>
+          <div className="header-top"></div>
+          <div className="header-bottom"></div>
+        </div>
+      </div>
+      <div className="layout-menu">
+        <button>Btn 1</button>
+        <button>Btn 2</button>
+        <button>Btn 3</button>
+        <button>Btn 4</button>
+        <button>Btn 5</button>
+      </div>
+      <div className="layout-sidebar-left">
+        <div className="sticky">
+          <div>
+            left Ad 1
+          </div>
+          <div>
+            left Ad 2
+          </div>
+        </div>
+      </div>
+      <div className="layout-main glass-dark">
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        <p>Lorem ipsum.</p>
+        ...
+      </div>
+      <div className="layout-sidebar-right">
+        <div className="sticky">
+          <div>
+            right Ad 1
+          </div>
+          <div>
+            right Ad 2
+          </div>
+        </div>
+      </div>
+      <div className="layout-footer">footer</div>
+    </div>
+  )
+}
+
+export default App
